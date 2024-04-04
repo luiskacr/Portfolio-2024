e31cdd6791a1519e2dc442d0f1c067b5eda1957e
Luis Carlos Porras Castro <74470666+luiskacr@users.noreply.github.com>
2024-04-04 15:54:43 -0600

    Route Fix

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,10 @@ function App() {
   return (
     <Routes>
       <Route element={<Layout/>} >
-      <Route path='/' element={<Home/>} />
-      <Route path='/projects' element={<Projects/>} />
-      <Route path='/contact' element={<Contact/>} />  
-       
-      </Route>
+        <Route path='/' element={<Home/>} />
+        <Route path='/projects' element={<Projects/>} />
+        <Route path='/contact' element={<Contact/>} />  
+        </Route>
       <Route path='*' element={<ErrorPage/>} /> 
     </Routes>
   )
